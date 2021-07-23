ce17a913c3aafe7f39711895bcdfc044cbb4cb54
Henrique Petrelli <henriquepetrelli96@gmail.com>
2021-07-22 22:13:31 -0300

    feat(navbar): component navbar created with variables of colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from 'src/components/navbar/navbar.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
