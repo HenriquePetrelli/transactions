23fb3381e9b6c5a74a798a6e9edc5dcaf19081aa
Henrique Petrelli <henriquepetrelli96@gmail.com>
2021-07-24 13:21:18 -0300

    feat(transaction-list): added transaction list

--- src/shared/components/transaction-list/transaction-list.component.ts
+++ src/shared/components/transaction-list/transaction-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ServerData } from 'src/shared/interfaces/server-data';
 import { Transaction } from 'src/shared/interfaces/transaction';
 import { TransactionService } from 'src/shared/services/transaction.service';
+import { Status } from 'src/shared/utils/enums/transactions';
 
 @Component({
   selector: 'app-transaction-list',
@@ -9,22 +10,31 @@ import { TransactionService } from 'src/shared/services/transaction.service';
   styleUrls: ['./transaction-list.component.less']
 })
 export class TransactionListComponent implements OnInit {
-  transactions: Transaction[] = [];
+  transactions: any;
+  enumStatus: any;
   constructor(private _service: TransactionService) {
+    this.enumStatus = Status;  
    }
 
   async ngOnInit(): Promise<void> {
     await this.getTransactionList();
   }
 
-  async getTransactionList() {
+  async getTransactionList() {      
    await this._service.getTransactions("transactions")
     .then(async (response: ServerData) => {
-     this.transactions.push(response.data);
-      console.log(this.transactions)
+     this.transactions = response.data; 
+    //  this.translateStatus();
     }).catch(()=> {
       console.log("ERRO!!");
     })
   }
+
+  // translateStatus() {
+  //   this.transactions.forEach((item: { status: string; }) => {
+  //     let transactionStatus = item.status;
+  //     item.status = this.enumStatus.transactionStatus;
+  //   });
+  // }
 
 }
---
+++ src/shared/utils/enums/transactions.ts
@@ -0,0 +1,5 @@
+export enum Status {
+    processed = "Processado",
+    processing = "Processando",
+    created = "Criado"
+}
