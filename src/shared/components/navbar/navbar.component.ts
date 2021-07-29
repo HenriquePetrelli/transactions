7bc98bb8a22d763c16d7352f596b9c599d907ae2
Henrique Petrelli <henriquepetrelli96@gmail.com>
2021-07-28 23:06:55 -0300

    wip(translate-language): added layout for switch languages

--- src/shared/components/navbar/navbar.component.ts
+++ src/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingService } from 'src/shared/services/loading/loading.service';
+import { NotificationService } from 'src/shared/services/notification/notification.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.less']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  isEnLanguage: string;
+  constructor(private notificationService: NotificationService, private loadingService: LoadingService) {
+    this.isEnLanguage = "0";
   }
 
+  ngOnInit(): void {
+    this.verifyLocalStorageLanguage();
+  }
+
+  async verifyLocalStorageLanguage() {
+    await this.loadingService.showLoading();
+    let lSLanguage = await localStorage.getItem('country');
+    this.isEnLanguage =  lSLanguage? lSLanguage : "0";
+    await this.loadingService.hideLoading();
+  }
+
+  async setCountry(isEn: string) {
+    await this.loadingService.showLoading();
+    await localStorage.setItem('country', isEn);
+    this.isEnLanguage = isEn;
+    await this.loadingService.hideLoading();
+    window.location.href = ('/');
+  }
 }
